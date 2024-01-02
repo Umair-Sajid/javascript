899393f68a7997a7eee5a237ed05b0d79d839872
Umair Sajid <umair.sajid323@gmail.com>
2024-01-02 23:52:53 +0500

    Your descriptive commit message here

---
+++ fun.js
@@ -0,0 +1,12 @@
+// function addTwoNumbers(num1, num2) {
+//   console.log(num1 + num2);
+// }
+// const result = addTwoNumbers(3, 5);
+// console.log(result);
+
+function addTwoNumbers(num1, num2) {
+  let result = num1 + num2;
+  return result;
+}
+const result = addTwoNumbers(3, 5);
+console.log(result);
